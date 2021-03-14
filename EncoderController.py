62d04a48553b6e4e868bd1135e13b423f211a72c
Brandi Whiner <pops.icle@sissyspacecraftsociety.net>
2021-03-14 10:23:53 -0500

    removed easter egg comment

--- EncoderController.py
+++ EncoderController.py
@@ -683,7 +683,7 @@ class EncoderController(MackieC4Component):
 
             # if a default Live device is chosen, iterate the DEVICE_DICT constant
             # to reorder the local list of plugin parameters
-            if self.__chosen_plugin.class_name in DEVICE_DICT.keys():  # MS should we import device dict from generic devices or Live?
+            if self.__chosen_plugin.class_name in DEVICE_DICT.keys():
                 device_banks = DEVICE_DICT[self.__chosen_plugin.class_name]
                 for bank in device_banks:
                     for param_name in bank:
