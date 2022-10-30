0e41268414b2d0f4199ee72fcc6d8f268d3249d2
Brandi Whiner <pops.icle@sissyspacecraftsociety.net>
2022-10-29 22:53:16 -0500

    removed unused references

--- wip/V2C4/C4Controller.py
+++ wip/V2C4/C4Controller.py
@@ -3,9 +3,7 @@ from .V2C4Component import *
 if sys.version_info[0] >= 3:  # Live 11
     from builtins import range, str
 
-from .C4_DEFINES import *
 from .C4Model import C4Model
-from _Framework.PhysicalDisplayElement import PhysicalDisplayElement
 
 
 class C4Controller(V2C4Component):
@@ -14,9 +12,5 @@ class C4Controller(V2C4Component):
 
     def __init__(self, *a, **k):
         V2C4Component.__init__(self, *a, **k)
-        # self.__lcd_00 = PhysicalDisplayElement(*a, **k)
-        # self.__lcd_01 = PhysicalDisplayElement(*a, **k)
-        # self.__lcd_02 = PhysicalDisplayElement(*a, **k)
-        # self.__lcd_03 = PhysicalDisplayElement(*a, **k)
 
 
