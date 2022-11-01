f8856535fe54e0207083e0c2fe4024fd983ab57a
Brandi Whiner <pops.icle@sissyspacecraftsociety.net>
2022-11-01 18:37:53 -0500

    changed file name and inheritance but still not using

---
+++ wip/V2C4/C4ControlSurfaceComponent.py
@@ -0,0 +1,19 @@
+#
+# from _Framework.ControlSurfaceComponent import ControlSurfaceComponent
+#
+#
+# class C4ControlSurfaceComponent(ControlSurfaceComponent):
+#     """ Base class for C4EncodersComponent and any other fully customized components
+#         inheriting classes should override these event handler methods if they care
+#         to know when the associated events occur
+#
+#         def on_selected_scene_changed(self):
+#         def on_selected_track_changed(self):
+#         def on_scene_list_changed(self):
+#         def on_track_list_changed(self):
+#     """
+#
+#     __module__ = __name__
+#
+#     def __init__(self):
+#         ControlSurfaceComponent.__init__(self)
