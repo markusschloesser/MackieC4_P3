3b275b7ab1c10986a412017924b2881c91b864ce
Brandi Whiner <pops.icle@sissyspacecraftsociety.net>
2022-12-25 14:15:59 -0600

    updated comments adjusted indentation

--- wip/V2C4/C4ElementFactory.py
+++ wip/V2C4/C4ElementFactory.py
@@ -19,9 +19,9 @@ from .C4EncoderElement import C4EncoderElement
 # The 32 + 19 "buttons" (and their associated "LEDs") are addressed using midi Note messages
 #
 # The 32 "encoder knobs" and 32 "encoder buttons" use the same message number, but CC versus Note message type
-# (0xB0, 0x20, 0x7F) is a CC message from encoder knob 0 with CC value 127
-# (0x90, 0x20, 0x7F) is a Note message from encoder button 0 with Note value (velocity) 127
-# (0xB0, 0x20, 0x7F) is also a CC message TO encoder LED Ring 0 with CC value 127
+# (0xB0, 0x00, 0x7F) is a CC message FROM encoder knob 0 with CC value 127
+# (0x90, 0x20, 0x7F) is a Note message FROM encoder button 0 with Note value (velocity) 127
+# (0xB0, 0x20, 0x7F) is a CC feedback message TO encoder LED Ring 0 with CC value 127
 #
 #
 # scratch comments
@@ -50,7 +50,7 @@ class C4ElementFactory:
         if self.midi_map[MIDI_CC_TYPE][cc_id] is None:
             name = 'Encoder_Control_%d' % cc_id
             self.midi_map[MIDI_CC_TYPE][cc_id] = EncoderElement(MIDI_CC_TYPE, C4_MIDI_CHANNEL, cc_id,
-                                                              Live.MidiMap.MapMode.relative_signed_bit, *a, **k)
+                                                                Live.MidiMap.MapMode.relative_signed_bit, *a, **k)
         else:
             assert False
         return self.midi_map[MIDI_CC_TYPE][cc_id]
@@ -69,24 +69,11 @@ class C4ElementFactory:
     def make_button(self, note_id=C4BTN_SPLIT_NOTE_ID, channel=0, is_momentary=True, *a, **k):
         if self.midi_map[MIDI_NOTE_TYPE][note_id] is None:
             self.midi_map[MIDI_NOTE_TYPE][note_id] = ButtonElement(is_momentary, MIDI_NOTE_TYPE,
-                                                                 channel, note_id, *a, **k)
+                                                                   channel, note_id, *a, **k)
         else:
             assert False
         return self.midi_map[MIDI_NOTE_TYPE][note_id]
 
-    # def make_button(self, identifier, channel=0, is_momentary=True, *a, **k):
-    #     #   is_momentary: True because C4 buttons send a message on being released
-    #     # MIDI_NOTE_TYPE: because C4 buttons send and receive "Midi Note" messages (0x90 id byte)
-    #     #        channel: 0 because the C4 communicates on channel 0
-    #     return ButtonElement(is_momentary, MIDI_NOTE_TYPE, channel, identifier, *a, **k)
-    #
-    # def make_encoder(self, identifier, name=None, *a, **k):
-    #     return C4EncoderElement(identifier, name, *a, **k)
-    #
-    # def make_framework_encoder(self, identifier, *a, **k):
-    #     return EncoderElement(MIDI_CC_TYPE, C4_MIDI_CHANNEL, identifier,
-    #                           Live.MidiMap.MapMode.relative_signed_bit, *a, **k)
-    #
     # def make_encoder_and_button(self, common_identifier, channel=0, is_momentary=True, name='', *a, **k):
     #     # if len(name) < 1:
     #     #     name = 'Encoder'
