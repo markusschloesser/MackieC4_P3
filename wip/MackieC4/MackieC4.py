bfd608960a5225368abff5a486e8cacdd386f853
Treegoatee <pops.icle@sissyspacecraftsociety.net>
2026-01-15 11:41:25 -0600

    fixed typo in property setter

--- wip/MackieC4/MackieC4.py
+++ wip/MackieC4/MackieC4.py
@@ -756,7 +756,7 @@ class MackieC4(MackieC4ListenerMixin, object):
 
     @last_selected_callback_type_index.setter
     def last_selected_callback_type_index(self, index):
-        self.last_selected_callback_type_index = index
+        self._last_selected_callback_type_index = index
 
     def scene_change(self): 
         selected_scene = self.song().view.selected_scene
