f2e59c5014390985d12573e265051ecc2873b12c
Treegoatee <pops.icle@sissyspacecraftsociety.net>
2026-04-07 16:25:03 -0500

    If the number of device parameters mod 24 is > 0, add one more required parameter bank
    to account for the last partial bank of parameters

--- wip/MackieC4/EncoderAssignmentHistory.py
+++ wip/MackieC4/EncoderAssignmentHistory.py
@@ -129,6 +129,8 @@ class ActiveDevice:
         self._selected_parameter_index = selected_parameter_index
         """ index of this device's selected parameter in the device's parameter list """
         self._parameter_bank_count = math.ceil(parameter_count // SETUP_DB_PARAM_BANK_SIZE)
+        if parameter_count % SETUP_DB_PARAM_BANK_SIZE > 0:
+            self._parameter_bank_count += 1
         """ the number of parameter banks worth of parameters in the device's parameter list (0 unless the device has more than 24 parameters) """
         self._selected_parameters_bank_index = selected_parameter_index % SETUP_DB_PARAM_BANK_SIZE
         """ the index of the parameter bank where the selected parameter would fall (0 - 23 unless SETUP_DB_PARAM_BANK_SIZE changes) """        
